37fb7293cc86646f22f290cff90e891100082e6e
Tanveer <tanvirkalim123@gmail.com>
2024-03-04 19:33:36 +0530

    first commit

---
+++ GetRandomDogImg/app.js
@@ -0,0 +1,20 @@
+// Dog image Api
+let url = "https://dog.ceo/api/breeds/image/random"
+let btn = document.querySelector("button");
+
+btn.addEventListener('click', async() => {
+    let link = await getImg();
+    let img = document.querySelector("img");
+    img.setAttribute("src", link)
+})
+
+async function getImg() {
+    try {
+        let res = await axios.get(url);
+        return res.data.message;
+    }
+    catch (e) {
+        console.log('error', e);
+        return "/"
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Activity with APIs.
+1 Get Random Img of cute DOG.
+2 Search Indian Collges based on States.
+3 Get Random Joke & see a cat who can talk .
